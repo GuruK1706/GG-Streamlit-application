3b2dbf1266e8b825583f52a06c16cb3b130ddc03
GuruK1706 <gkeerthana17@gmail.com>
2026-03-06 19:51:04 +0000

    User input handling

--- Basic Stream Lit Application/FirstStreamLitApp.py
+++ Basic Stream Lit Application/FirstStreamLitApp.py
@@ -10,3 +10,21 @@ st.write("Current date and time:", datetime.now().strftime("%Y-%m-%d %H:%M: %S")
 #Create button
 if st.button("Click Me"):
     st.write("You clicke the button!")
+
+#Write header and subheader
+st.header("Basic Streamlit Header")
+st.subheader("Widgets")
+
+#Create number slider widget
+number = st.slider("Pick a number", 0, 100, 25)
+st.write("You picked:", number)
+
+#Create text input widget
+text = st.text_input("Enter some text")
+st.write("You typed:", text)
+
+#Create multiselect widget
+options = st.multiselect("Choose your pizza toppings", ["Chesse","Mushrooms","Onions"])
+st.write("Toppings ", options)
+
+
